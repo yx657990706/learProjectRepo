f728bce4def99b4cacbf482f084cc36511d58825
殷雄 <657990706@qq.com>
2018-04-22 16:21:24 +0800

    尝试在控制台输出彩色日志,不过在mac上表现为所有颜色处理的地方都不显示了.原因未知,尝试在window下运行下..

--- nas/src/test/java/com/leo/nas/BaseJunitTest.java
+++ nas/src/test/java/com/leo/nas/BaseJunitTest.java
@@ -1,5 +1,6 @@
 package com.leo.nas;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,4 +18,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //@WebAppConfiguration
 public class BaseJunitTest {
 
+	@Test
+	public void demo() {
+	}
 }
--- nas/src/test/java/com/leo/nas/JustTest.java
+++ nas/src/test/java/com/leo/nas/JustTest.java
@@ -4,22 +4,26 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 日志级别测试
  * @author yinxiong
  *
  */
+@Slf4j
 public class JustTest extends BaseJunitTest{
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	//private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Test
 	public void outLogger() {
 		//级别排序为： TRACE < DEBUG < INFO < WARN < ERROR
-		logger.trace("======TRACE======");
-		logger.debug("======debug======");
-		logger.info("======info======");
-		logger.warn("======warn======");
-		logger.error("======error======");
+		log.trace("======TRACE======");
+		log.debug("======debug======");
+		log.info("======info======");
+		log.warn("======warn======");
+		log.error("======error======");
+		System.out.println("Hello \u001b[1;31mred\u001b[0m world!");
 	}
 }
