747fc7d4115544c7966803dbc49624039ed55be6
殷雄 <657990706@qq.com>
2018-04-22 10:48:10 +0800

    增加druid的监控配置,用于监控sql访问及数据源情况.访问地址:localhost:8080/nas/druid 用户名和密码是DuidStatViewServlet.java中配置的

--- nas/src/main/java/com/leo/nas/NasApplication.java
+++ nas/src/main/java/com/leo/nas/NasApplication.java
@@ -1,14 +1,11 @@
 package com.leo.nas;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan //spring能够扫描到我们自己编写的servlet和filter
+@ServletComponentScan //spring能够扫描到我们自己编写的servlet和filter,druid需要设置该项
 @SpringBootApplication
-//这里是扫描dao接口的包用于识别mybatis
-//@MapperScan(basePackages="com.leo.nas.*.dao")
 public class NasApplication {
 
 	public static void main(String[] args) {
--- nas/src/main/java/com/leo/nas/servlert/DruidStatViewServlet.java
+++ nas/src/main/java/com/leo/nas/servlert/DruidStatViewServlet.java
@@ -15,7 +15,6 @@ import com.alibaba.druid.support.http.StatViewServlet;
         initParams= {  
         	        @WebInitParam(name = "allow", value = "192.168.16.110,127.0.0.1"), // IP白名单// (没有配置或者为空，则允许所有访问)  
         	        @WebInitParam(name = "deny", value = "192.168.1.111"), // IP黑名单// (存在共同时，deny优先于allow)  
-                @WebInitParam(name="allow",value="127.0.0.1"),  
                 @WebInitParam(name="loginUsername",value="root"),  
                 @WebInitParam(name="loginPassword",value="root"),  
                 @WebInitParam(name="resetEnable",value="true")// 允许HTML页面上的“Reset All”功能  
